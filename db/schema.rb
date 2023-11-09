6385ac5b4a14b3120a1fd572db38b305c1f25086
Yun Choi <cyss0317@gmail.com>
2023-11-08 21:56:42 -0600

    remove day model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_08_042636) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "days", force: :cascade do |t|
-    t.integer "month_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["month_id"], name: "index_days_on_month_id"
-    t.index ["user_id"], name: "index_days_on_user_id"
-  end
 
 end
