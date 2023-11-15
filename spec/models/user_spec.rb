d42a9423ccb32ddd57648843260f2a43b93dd679
Yun Choi <cyss0317@gmail.com>
2023-11-14 23:22:53 -0600

    add model validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'ensures duplicated email address' do
+      create(:user, email: 'test@test.com')
+      dup_user = User.new(email: 'test@test.com').save
+
+      expect(dup_user).to eq(false)
+    end
+
+    it 'ensures email presence' do
+      user = User.new(first_name: 'First', middle_name: 'Middle', last_name: 'Last', email: '',
+                      password: '12341234').save
+      expect(user).to eq(false)
+    end
+  end
 end
