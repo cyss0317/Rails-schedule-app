411979f4393b1de9093f554616e7e0d6dfd0f283
Yun Choi <cyss0317@gmail.com>
2023-12-26 23:01:31 -0600

    before ripping out simple calendar css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@
 
 class HomeController < ApplicationController
   def index
+    Rails.logger.info "ip address: #{request.remote_ip}"
+    Rails.logger.info "X-Forwarded-For: #{request.headers['X-Forwarded-For']}"
+    Rails.logger.info "HTTP_X_REAL_IP: #{request.headers['HTTP_X_REAL_IP']}"
+
     params.fetch(:start_date, Date.today).to_date
     @meetings = Meeting.default
     current_user ? render : (redirect_to new_user_registration_path)
