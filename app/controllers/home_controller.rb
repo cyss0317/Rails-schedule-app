f20f2112e671f435c40bd6207574bbc6bff7788a
Yun Sung Choi <83522307+cyss0317@users.noreply.github.com>
2023-12-27 23:37:57 -0600

    Responsive design added for weekly and monthly views (#7)
    * before ripping out simple calendar css
    
    * removed simple calendar css
    
    * changed width in header
    
    * before refactor
    
    * responsive design'
    
    * paired weekly and monthly view css
    
    * WIP
    
    * WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,12 @@
 
 class HomeController < ApplicationController
   def index
+    Rails.logger.info "ip address: #{request.remote_ip}"
+    Rails.logger.info "X-Forwarded-For: #{request.headers['X-Forwarded-For']}"
+    Rails.logger.info "HTTP_X_REAL_IP: #{request.headers['HTTP_X_REAL_IP']}"
+
     params.fetch(:start_date, Date.today).to_date
     @meetings = Meeting.default
-    current_user ? render : (redirect_to new_user_registration_path)
+    current_user ? render : (redirect_to new_user_session_path)
   end
 end
