55a6c47b2e560ea74da18ef9962dc462b627a03c
Yun Choi <cyss0317@gmail.com>
2024-01-04 00:02:33 -0600

    WIP

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,10 +1,15 @@
 FactoryBot.define do
   factory :location do
-    company_id { create(:company).id }
-
+    company
     name { Faker::Address.city }
     address { Faker::Address.street_address }
     phone_number { Faker::PhoneNumber.phone_number }
     ip_address { Faker::Internet.ip_v4_address }
+
+    trait :with_users do
+      after(:create) do |location|
+        location.users << create_list(:user, 3, location_id: location.id, company_id: location.company.id)
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,6 @@ RSpec.describe User, type: :model do
   describe 'associations' do
     it { should belong_to(:company) }
     it { should belong_to(:location) }
-    it { should have_many(:meetings).dependent(:destroy)}
+    it { should have_many(:meetings).dependent(:destroy) }
   end
 end
