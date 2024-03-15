a208558c6058b8f0b2c55644dd19125cb6543189
Yun Choi <cyss0317@gmail.com>
2024-03-14 22:31:00 -0500

    removed name presence validation from meeting

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,7 +7,6 @@ class Meeting < ApplicationRecord
 
   belongs_to :user
 
-  validates :name, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
   validate :start_time_cannot_be_greater_than_end_time
