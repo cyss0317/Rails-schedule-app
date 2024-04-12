3044263f2868fda01239b1d2dc812584a01a0571
Yun Choi <cyss0317@gmail.com>
2024-04-11 21:55:19 -0500

    added authenticate_user! before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
   # before_action :set_time_zone, if: :user_signed_in?
 
   protected
