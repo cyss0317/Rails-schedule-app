1d32d99fbefdf19e728e3c5f0ed576081f00a990
Yun Choi <cyss0317@gmail.com>
2024-04-15 22:11:59 -0500

    fixed day off request spec

--- spec/requests/day_offs_spec.rb
+++ spec/requests/day_offs_spec.rb
@@ -3,34 +3,42 @@
 require 'rails_helper'
 
 RSpec.describe 'DayOffs', type: :request do
+  include Devise::Test::IntegrationHelpers  # Corrected include here
+
+  let!(:user) { create(:user) }
+  let(:valid_params) do
+    {
+      'day_offs' => {
+        'start_time' => '2020-01-01 12:00:00 -0600',
+        'end_time' => '2020-01-01 14:00:00 -0600',
+        'user_id' => user.id
+      }
+    }
+  end
+  let(:invalid_params) do
+    {
+      'day_offs' => {
+        'start_time' => '',
+        'end_time' => '',
+        'user_id' => user.id
+      }
+    }
+  end
+  
   describe '#create' do
-    let!(:user) { create(:user) }
-    let(:valid_params) do
-      {
-        'day_offs' => {
-          'start_time' => '2020-01-01 12:00:00 -0600',
-          'end_time' => '2020-01-01 14:00:00 -0600',
-          'user_id' => user.id
-        }
-      }
-    end
-    let(:invalid_params) do
-      {
-        'day_offs' => {
-          'start_time' => '',
-          'end_time' => '',
-          'user_id' => user.id
-        }
-      }
-    end
 
     it 'creates a new day off when valid params are given' do
       expect do
+        sign_in user  # This method is now properly supported
+
         post day_offs_path, params: valid_params
       end.to change(DayOff, :count).by(1)
     end
+
     it 'does NOT create a new day off when invalid params are given' do
       expect do
+        sign_in user  # Sign in added here for consistency
+
         post day_offs_path, params: invalid_params
       end.to change(DayOff, :count).by(0)
     end
