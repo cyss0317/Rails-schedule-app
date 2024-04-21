f41b3c058ffb785a3c9a08191fc06c7059bd2187
Yun Choi <cyss0317@gmail.com>
2024-04-20 23:01:53 -0500

    fixed monthly meeting filter method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,9 @@ class HomeController < ApplicationController
     Rails.logger.info "X-Forwarded-For: #{request.headers['X-Forwarded-For']}"
     Rails.logger.info "HTTP_X_REAL_IP: #{request.headers['HTTP_X_REAL_IP']}"
 
-    params.fetch(:start_date, Date.today).to_date
-    @meetings = Meeting.current_week_meetings
+    start_date = params.fetch(:start_date, Date.today).to_date
+
+    @meetings =  Meeting.monthly_meetings(start_date)
     current_user ? render : (redirect_to new_user_session_path)
   end
 end
