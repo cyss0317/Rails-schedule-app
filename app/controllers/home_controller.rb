9a991b0cd28d590bf4075492dd780a6bb7c35b67
Yun Choi <cyss0317@gmail.com>
2025-10-01 14:00:34 -0500

    Add filter location users by active status

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     Rails.logger.info "HTTP_X_REAL_IP: #{request.headers['HTTP_X_REAL_IP']}"
     return redirect_to(new_user_session_path) unless current_user
 
-    company_id = current_user.developer_user? ? Company.first : (current_user.companies&.first&.id || current_user.working_companies&.first&.id)
+    company_id = current_user.developer_user? ? Company.first : (current_user.companies&.first&.id || current_user.active_working_companies&.first&.id)
 
     if current_user.admin_user? && (current_user.companies.empty? && current_user.working_companies.empty?)
       flash[:notice] = "Let's setup your company first"
